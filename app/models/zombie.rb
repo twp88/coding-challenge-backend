b5f0796e0a80df1eed6cc7716ff6220155296890
Tom <tom.w.page@gmail.com>
2018-10-26 15:57:15 +0200

    Add validations to model

--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -1,2 +1,11 @@
 class Zombie < ApplicationRecord
+  before_save :set_defaults
+
+  validates :name, presence: true
+
+  def set_defaults
+    self.hit_points = 10 if self.hit_points.nil?
+    self.brains_eaten = 5 if self.brains_eaten.nil?
+    self.speed = 15 if self.speed.nil?
+  end
 end
