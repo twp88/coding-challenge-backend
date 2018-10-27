546baa0a490205e797a8926d243b6603b87b9640
Tom <tom.w.page@gmail.com>
2018-10-27 17:09:49 +0200

    Add test

--- spec/models/armor_spec.rb
+++ spec/models/armor_spec.rb
@@ -11,13 +11,28 @@ RSpec.describe Armor, type: :model do
 
   context 'relations tests' do
     let(:shield) { create(:armor, id: 1) }
-    let(:other_shield) { create(:armor, id: 2) }
+    let(:other_shield) { create(:armor, id: 2, name: 'other_shield') }
     let(:other_zombie) { create(:zombie, id: 2) }
-    let!(:wardrobe) { create(:wardrobe, zombie_id: zombie.id, armor_id: shield.id) }
     let(:zombie) { create(:zombie, id: 1) }
+
+    let!(:wardrobe) do
+      create(:wardrobe,
+             zombie_id: zombie.id,
+             armor_id: shield.id)
+    end
+
+    let!(:other_wardrobe) do
+      create(:wardrobe,
+             zombie_id: other_zombie.id,
+             armor_id: other_shield.id)
+    end
 
     it 'returns the correct armor' do
       expect(zombie.armors.first.name).to eq(shield.name)
+    end
+
+    it "doesn't return incorrect armor" do
+      expect(zombie.armors.first.name).not_to eq(other_shield.name)
     end
   end
 end
