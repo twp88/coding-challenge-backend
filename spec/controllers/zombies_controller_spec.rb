175ff9677c2e28052c2e2e3bf95371fe71c40e45
Tom <tom.w.page@gmail.com>
2018-10-27 20:02:17 +0200

    Add test for show endpoint

--- spec/controllers/zombies_controller_spec.rb
+++ spec/controllers/zombies_controller_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe ZombiesController, type: :controller do
+  let!(:first_zombie) { create(:zombie) }
+  let!(:second_zombie) { create(:zombie) }
+
   describe '#index' do
     subject { get :index }
-
-    let!(:first_zombie) { create(:zombie) }
-    let!(:second_zombie) { create(:zombie) }
 
     it { is_expected.to be_successful }
 
@@ -13,6 +13,18 @@ RSpec.describe ZombiesController, type: :controller do
       get :index
 
       expect(response.body).to eq([first_zombie, second_zombie].to_json)
+    end
+  end
+
+  describe '#show' do
+    subject { get :show, params: { id: first_zombie.id } }
+
+    it { is_expected.to be_successful }
+
+    it 'returns correct json' do
+      get :show, params: { id: first_zombie.id }
+
+      expect(response.body).to eq([first_zombie].to_json)
     end
   end
 end
