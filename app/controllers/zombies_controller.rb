584baf68163e814a07af7242ef9bbc6e8e96dfaf
Tom <tom.w.page@gmail.com>
2018-10-28 17:13:06 +0100

    Add 404 code

--- app/controllers/zombies_controller.rb
+++ app/controllers/zombies_controller.rb
@@ -1,4 +1,9 @@
 class ZombiesController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound do
+    logger.error "Woops! This zombie doesn't exist!"
+    render file: "#{Rails.root}/public/404.html", status: 404
+  end
+
   def index
     @zombies = Zombie.all
     render json: @zombies
