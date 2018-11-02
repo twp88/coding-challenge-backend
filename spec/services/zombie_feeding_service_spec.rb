a47636a15b7df5729118b739eaf153af4105cfd9
Tom <tom.w.page@gmail.com>
2018-11-02 21:09:13 +0100

    Add extra test

--- spec/services/zombie_feeding_service_spec.rb
+++ spec/services/zombie_feeding_service_spec.rb
@@ -12,5 +12,11 @@ describe ZombieFeedingService do
         .to change(Person, :count)
         .by(-1)
     end
+
+    it 'increases zombie hp' do
+      expect { subject.call(person, hungry_zombie) }
+        .to change(hungry_zombie, :hp)
+        .by(person.hp)
+    end
   end
 end
