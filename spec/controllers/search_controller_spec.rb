140f926844fdb79593ee0f2c1c439616ba1958bb
Tom <tom.w.page@gmail.com>
2018-11-02 15:16:03 +0100

    Add model and controller search tests

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe SearchController, type: :controller do
+RSpec.describe SearchController, type: :controller, elasticsearch: true do
+  let!(:zombie) { Zombie.create(name: Faker::HarryPotter.unique.character) }
 
+  let(:response_name) { "\"name\":\"#{zombie.name}\"" }
+
+  describe '#search' do
+    subject { get :search, params: { q: zombie.name } }
+
+    it { is_expected.to be_successful }
+
+    context 'when searching a name' do
+      it 'returns correct zombie' do
+        Zombie.__elasticsearch__.refresh_index!
+        subject
+
+        expect(response.body.split(',')[5]).to eq(response_name)
+      end
+    end
+  end
 end
