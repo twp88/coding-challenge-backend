d3b64e58a41e78b5b9095d14c44780fd0177c732
Tom <tom.w.page@gmail.com>
2018-11-02 23:49:10 +0100

    Refactor service

--- app/services/zombie_feeding_service.rb
+++ app/services/zombie_feeding_service.rb
@@ -1,9 +1,19 @@
 class ZombieFeedingService
   def call(food, zombie)
+    change_zombie(food, zombie)
+    destroy_food(food)
+  end
+
+  private
+
+  def change_zombie(food, zombie)
     zombie.hit_points += food.hp
     zombie.brains_eaten += 1
     zombie.hungry = false
     zombie.last_ate = Date.today
+  end
+
+  def destroy_food(food)
     food.destroy
   end
 end
