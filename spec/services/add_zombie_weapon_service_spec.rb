54eb760e7e620c345aafaf434b45fafd4629f21e
Tom <tom.w.page@gmail.com>
2018-11-03 17:37:19 +0100

    Add check test

--- spec/services/add_zombie_weapon_service_spec.rb
+++ spec/services/add_zombie_weapon_service_spec.rb
@@ -7,8 +7,19 @@ RSpec.describe AddZombieWeaponService do
 
   context 'when called' do
     it 'adds weapon to zombie' do
-
       expect { subject.call }.to change(Armory, :count).by(1)
+    end
+  end
+
+  context 'when called with the same weapon' do
+    let!(:armory) do
+      create(:armory,
+             zombie_id: zombie.id,
+             weapon_id: weapon.id)
+    end
+
+    it 'does not add weapon' do
+      expect(subject.call).to eq("Zombie can't have the same weapon twice!")
     end
   end
 end
