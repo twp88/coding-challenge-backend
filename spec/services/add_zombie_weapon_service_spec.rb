c26f13cdd546ad759e36edc42da88730808b3b0b
Tom <tom.w.page@gmail.com>
2018-11-03 17:47:49 +0100

    Add test for maximum no weapons

--- spec/services/add_zombie_weapon_service_spec.rb
+++ spec/services/add_zombie_weapon_service_spec.rb
@@ -22,4 +22,32 @@ RSpec.describe AddZombieWeaponService do
       expect(subject.call).to eq("Zombie can't have the same weapon twice!")
     end
   end
+
+  context 'when called with too many weapons' do
+    let!(:shotgun) { create(:weapon, name: 'Shotgun') }
+    let!(:machete) { create(:weapon, name: 'Machete') }
+    let!(:pistol) { create(:weapon, name: 'Pistol') }
+
+    let!(:armory) do
+      create(:armory,
+             zombie_id: zombie.id,
+             weapon_id: shotgun.id)
+    end
+
+    let!(:armory) do
+      create(:armory,
+             zombie_id: zombie.id,
+             weapon_id: machete.id)
+    end
+
+    let!(:armory) do
+      create(:armory,
+             zombie_id: zombie.id,
+             weapon_id: pistol.id)
+    end
+
+    it 'does not add weapon' do
+      expect(subject.call).to eq("Zombie can only have a maximum four weapons")
+    end
+  end
 end
