1941d500ba889e9cadfe8590c595a8c38456d4b7
Tom <tom.w.page@gmail.com>
2018-11-03 17:37:37 +0100

    Add check code

--- app/services/add_zombie_weapon_service.rb
+++ app/services/add_zombie_weapon_service.rb
@@ -5,8 +5,29 @@ class AddZombieWeaponService
   end
 
   def call
+    assign_variables
+
+    repeat_weapon_check? ? error_message : create_weapon
+  end
+
+  private
+
+  def assign_variables
     @zombie = Zombie.find(@zombie_id)
     @weapon = Weapon.find_by(name: @weapon_name)
+  end
+
+  def repeat_weapon_check?
+    return false if Armory.where(zombie_id: @zombie_id,
+                                 weapon_id: @weapon.id).empty?
+    true
+  end
+
+  def create_weapon
     Armory.create(zombie_id: @zombie.id, weapon_id: @weapon.id)
+  end
+
+  def error_message
+    "Zombie can't have the same weapon twice!"
   end
 end
