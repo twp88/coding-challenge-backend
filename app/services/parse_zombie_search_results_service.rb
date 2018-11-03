f7d02ecf3a5179f463815a719c1a2df8345df4a9
Tom <tom.w.page@gmail.com>
2018-11-03 11:08:49 +0100

    Quick check

--- app/services/parse_zombie_search_results_service.rb
+++ app/services/parse_zombie_search_results_service.rb
@@ -1,3 +1,19 @@
 class ParseZombieSearchResultsService
+  def initialize(zombies)
+    @zombies = zombies
+  end
 
+  def call
+
+  end
+
+  private
+
+  def create_array_for_each_zombie
+    @return_array = @zombies.
+  end
+
+  def find_each_zombie
+    @zombies.map { |z| z.id }
+  end
 end
