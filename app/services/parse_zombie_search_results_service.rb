d6e48beebc8463ab22ee4e3cce652e660b03abb2
Tom <tom.w.page@gmail.com>
2018-11-03 10:08:18 +0100

    Improved test

---
+++ app/services/parse_zombie_search_results_service.rb
@@ -0,0 +1,3 @@
+class ParseZombieSearchResultsService
+
+end
--- spec/services/parse_zombie_search_results_service_spec.rb
+++ spec/services/parse_zombie_search_results_service_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 RSpec.describe ParseZombieSearchResultsService do
   describe 'ParseZombieSearchResultsService' do
-    subject { ParseZombieSearchResultsService.new(zombies) }
+    subject { ParseZombieSearchResultsService.new([zombie]) }
 
     let(:zombie) { create(:zombie) }
     let(:shield) { create(:armor) }
     let(:weapon) { create(:weapon) }
+
+    let(:desired_result) do
+      "Zombie #{zombie.name}: #{zombie},
+       Zombie weapons: #{zombie.weapons},
+       Zombie armor: #{zombie.armors} ||"
+    end
 
     let!(:armory) do
       create(:armory,
@@ -22,7 +28,7 @@ RSpec.describe ParseZombieSearchResultsService do
 
     context 'when called' do
       it 'returns correctly parsed results' do
-        debugger
+        expect(subject.call).to eq(desired_result)
       end
     end
   end
