c5aa50441f0cfaabf038f5e814655e48f4b122c9
Tom <tom.w.page@gmail.com>
2018-11-04 16:19:07 +0100

    Change search

--- app/models/armor.rb
+++ app/models/armor.rb
@@ -1,4 +1,9 @@
 class Armor < ApplicationRecord
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
   has_many :wardrobes
   has_many :zombies, through: :wardrobes
 end
+
+Armor.import
